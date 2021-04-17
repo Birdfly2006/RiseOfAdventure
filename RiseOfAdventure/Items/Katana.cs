15cc1e9d374695083331b40b98b1310e26aae681
pyrossal <63841160+pyrossal@users.noreply.github.com>
2021-04-17 22:36:23 +1100

    Update Katana.cs

--- RiseOfAdventure/Items/Katana.cs
+++ RiseOfAdventure/Items/Katana.cs
@@ -7,7 +7,7 @@ namespace RiseOfAdventure.Items
 	{
 		public override void SetStaticDefaults() 
 		{
-			// DisplayName.SetDefault("King's Katana"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+			// DisplayName.SetDefault("King's Katana"); {
 			Tooltip.SetDefault("An old katana that was made to protect kings...");
 		}
 
