16dbba68c5ff6e619e062e19b915cbef025ac2c5
pyrossal <63841160+pyrossal@users.noreply.github.com>
2021-04-18 19:52:13 +1100

    Add files via upload

---
+++ RiseOfAdventure/Items/Katana_Drop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace RiseOfAdventure.Items
+{
+    public class RiseOfAdventure : GlobalNPC
+    {
+        public override void NPCLoot(NPC npc)
+        {
+            if (npc.type == NPCID.KingSlime)
+            {
+                Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("Kings_Katana"), Main.rand.Next(1, 1));
+            }
+        }
+    }
+}
